9cf078afda7a982186bbcfd61dd2868a714f6bc9
Adam Swanwick <a.j.swanwick@gmail.com>
2024-11-07 16:22:06 +0000

    update return statements

--- ball.js
+++ ball.js
@@ -1,20 +1,13 @@
 
-
-  let buttons = document.getElementsByTagName("button");
-      
-  for (let button of buttons){
-    button.addEventListener("click", shakeBall())
-    
-       }
-
   
   
 
 
 
 
+  
   function shakeBall(){ 
-  const eightBall = 'Magic 8 Ball Says'
+  const eightBall = 'Zoltar Says:'
   let randomNumber = Math.floor(Math.random() *8)
  
   console.log(`${eightBall} your number is: ${randomNumber}`)
@@ -22,91 +15,32 @@
  
 
   switch(randomNumber){
-    case 0: return ( `${eightBall}- it is certain` );
-    case 1: console.log(`${eightBall}- it is decidedely so`);
-    break;
-    case 2: console.log (`${eightBall}-: Try again`);
-    break;
-    case 3: console.log(`${eightBall}- Cannot predict now`);
-    break;
-    case 4: console.log(`${eightBall}- Do not count on it`);
-    break;
-    case 5: console.log(`${eightBall}- My sources say no`);
-    break;
-    case 6: console.log(`${eightBall} outlook not so good`);
-    break;
-    case 7: console.log(`${eightBall} signs point to yes`);
-    break;
-  
-  
-}
-
-   }
-
-   
-/*function draw(){
-  fill(0,0,0)
-  circle(300,300,400);
-  fill(210,210,210)
-  square(200,200,200,10);
-
-  
-
-if( randomNumber === 0 ){
-  fill(0,0,0)
-    textSize(30)
-  textAlign(CENTER) 
-    text('It is certain',250,250, 80)
-}
- else if ( randomNumber === 1 ){
-  fill(0,0,0)
-    textSize(30)
-  textAlign(CENTER)
-    text('It is decidedly so',250,250, 80)
-} 
-  else if( randomNumber === 2 ){
-  fill(0,0,0)
-    textSize(30)
-  textAlign(CENTER)
-    text('It is certain',250,250, 80)
-  }
+    case 0: return document.getElementById('answer').innerText = ( `${eightBall}- it is certain` );
     
-   else if( randomNumber === 3 ){
-  fill(0,0,0)
-    textSize(30)
-  textAlign(CENTER)
-    text('Try again',250,250, 80)
-   }
-   else if( randomNumber === 4 ){
-  fill(0,0,0)
-  textSize(30)
-  textAlign(CENTER)
-    text('Cannot Predict now',250,250, 80)
-   }
-   else if( randomNumber === 5 ){
-  fill(0,0,0)
-    textSize(30)
-  textAlign(CENTER)
-    text('My sources say no',250,250, 100)
-   }
+    case 1: return  document.getElementById('answer').innerText = (`${eightBall}- it is decidedely so`);
+    
+    case 2: return document.getElementById('answer').innerText = (`${eightBall}-: Try again`);
   
-else if( randomNumber === 6 ){
-  fill(0,0,0)
-    textSize(30)
-  textAlign(CENTER)
-    text('Outlook not so good',250,250, 100)
+    case 3: return document.getElementById('answer').innerText = (`${eightBall}- Cannot predict now`);
+   
+    case 4: return document.getElementById('answer').innerText =(`${eightBall}- Do not count on it`);
+    
+    case 5: return document.getElementById('answer').innerText = (`${eightBall}- My sources say no`);
+   
+    case 6: return document.getElementById('answer').innerText =(`${eightBall} outlook not so good`);
+    
+    case 7: return document.getElementById('answer').innerText =(`${eightBall} signs point to yes`);
+    
+  
+  
 }
-  
-   else if( randomNumber === 7 ){
-  fill(0,0,0)
-  textSize(30)
-  textAlign(CENTER)
-  text('Signs point to yes',250,250, 80)
+
    }
+
+   let question = document.getElementById('question')
+   let answer = document.getElementById('answer')
+
+
+
   
-  
-  
-  
-  
-} 
-*/
+
